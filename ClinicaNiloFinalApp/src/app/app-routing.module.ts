03dbef4ba4e4c19a71e1d1e240ea5a8cf537dac2
mcruzlp <mcruzlp@gmail.com>
2022-05-03 23:52:22 +0200

    welcome design

--- ClinicaNiloFinalApp/src/app/app-routing.module.ts
+++ ClinicaNiloFinalApp/src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'welcome',
+    loadChildren: () => import('./pages/welcome/welcome.module').then( m => m.WelcomePageModule)
+  },
 ];
 
 @NgModule({
