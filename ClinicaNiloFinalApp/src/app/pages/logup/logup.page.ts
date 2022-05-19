45b2f71d8f5d4263d6aca6fdfff74d730387d01f
mcruzlp <mcruzlp@gmail.com>
2022-05-19 10:59:50 +0200

    registro ts

--- ClinicaNiloFinalApp/src/app/pages/logup/logup.page.ts
+++ ClinicaNiloFinalApp/src/app/pages/logup/logup.page.ts
@@ -1,4 +1,7 @@
+import { AlertController } from '@ionic/angular';
+import { AuthService } from 'src/app/services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logup',
@@ -8,7 +11,43 @@ import { Component, OnInit } from '@angular/core';
 export class LogupPage implements OnInit {
   title = 'Registro';
 
-  constructor() {}
+  email: string;
+  password: string;
+
+  constructor(
+    public authService: AuthService,
+    private router: Router,
+    private alertController: AlertController
+  ) {}
 
   ngOnInit() {}
+
+  async register() {
+    const registerSuccess = await this.authService.register(
+      this.email,
+      this.password
+    );
+    if (registerSuccess) {
+      this.router.navigateByUrl('/home');
+    } else {
+      this.presentAlert();
+    }
+  }
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Conexión fallida',
+      subHeader: 'No se ha podido completar el registro',
+      message:
+        'El correo electrónico y la contraseña proporcionados no son válidos.',
+      buttons: ['Aceptar'],
+    });
+    await alert.present();
+  }
+
+  /*
+register() {
+    this.authService
+      .register(this.email, this.password)
+      .then(() => this.router.navigateByUrl('/home'));
+  } */
 }
